c7b451e800f36bc578c1da7b16e26f0bf902153d
shadykun <soltuzraresflorin2004@gmail.com>
2025-07-02 00:38:24 +0300

    update: support folder input for analysis

--- analyze_quality.py
+++ analyze_quality.py
@@ -42,11 +42,14 @@ def analyze_quality(distorted_file_path, reference_file_path, output_file_name, 
     if not output_file_name:
         raise ValueError("Output file name must be provided.")
 
+    if not os.path.exists('analysis/' + output_file_name):
+        os.makedirs('analysis/' + output_file_name)
+
     current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_file_path_1 = f"analysis/{current_time}_psnr_{output_file_name}.txt"
-    output_file_path_2 = f"analysis/{current_time}_vmaf_{output_file_name}"
-    output_file_path_3 = f"analysis/{current_time}_ssim_{output_file_name}.txt"
-    output_file_path_4 = f"analysis/{current_time}_lpips_{output_file_name}.txt"
+    output_file_path_1 = f"analysis/{output_file_name}/{current_time}_psnr_{output_file_name}.txt"
+    output_file_path_2 = f"analysis/{output_file_name}/{current_time}_vmaf_{output_file_name}"
+    output_file_path_3 = f"analysis/{output_file_name}/{current_time}_ssim_{output_file_name}.txt"
+    output_file_path_4 = f"analysis/{output_file_name}/{current_time}_lpips_{output_file_name}.txt"
 
     command1 = [
         'ffmpeg', '-i', distorted_file_path, '-i', reference_file_path,
@@ -79,18 +82,58 @@ def analyze_quality(distorted_file_path, reference_file_path, output_file_name, 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred during quality analysis: {e}")
 
+def get_videos_list(directory):
+    """
+    Get a list of video files in the specified directory.
+    
+    :param directory: Directory to search for video files.
+    :return: List of video file paths.
+    """
+    if not os.path.exists(directory):
+        raise FileNotFoundError(f"Directory not found: {directory}")
 
+    video_extensions = ('.mp4', '.avi', '.mkv', '.mov')
+    # Ensure the list only contains the file names with the specified video extensions
+    return [
+        os.path.splitext(f)[0]
+        for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f)) and f.lower().endswith(video_extensions)
+    ]
 
 if __name__ == "__main__":
     # Example usage
-    distorted_file = "input/video/023_klingai_reedit.mp4"
-    reference_file = "results/023_klingai_reedit_20250627_124443.mp4"
-    output_name = "023_klingai_reedit"
+    output_name = "patient_1_L2"
+    distorted_file = f"input/video/{output_name}.mp4"
+    reference_file = f"results/{output_name}_20250701_133046.mp4"
 
-    width, height = get_video_resolution(reference_file)
-    rezolution = f"{width}:{height}"
+    input_list = get_videos_list('input/video')
+    input_list = sorted(input_list, key=lambda x: x.lower())
+    print("Input video files:")
+    for file in input_list:
+        print(file)
+
+    output_list = get_videos_list('results')
+    for output_file in output_list:
+        of = output_file[:-16]  # Remove the timestamp part from the filename
+        if of not in input_list:
+            print(f"Warning: Output file {output_file} does not have a corresponding input file in 'input/video' directory. Removing it from the output list.")
+            output_list.remove(output_file)
+
+    output_list = sorted(output_list, key=lambda x: x.lower())
+
+    print("Output video files:")
+    for file in output_list:
+        print(file)
 
     try:
-        analyze_quality(distorted_file, reference_file, output_name, rezolution)
+        for index in range(len(input_list)):
+            distorted_file = f"input/video/{input_list[index]}.mp4"
+            reference_file = f"results/{output_list[index]}.mp4"
+            output_name = input_list[index]
+
+            width, height = get_video_resolution(reference_file)
+            rezolution = f"{width}:{height}"
+
+            analyze_quality(distorted_file, reference_file, output_name, rezolution)
     except Exception as e:
         print(f"Error: {e}")
